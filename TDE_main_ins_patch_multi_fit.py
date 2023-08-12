dc1f0c5733a07f444942833b3ab1fb3558f8aa81
tang-agui <61316384+tang-agui@users.noreply.github.com>
2023-08-12 12:35:08 +0800

    Update TDE_main_ins_patch_multi_fit.py

--- TDE_main_ins_patch_multi_fit.py
+++ TDE_main_ins_patch_multi_fit.py
@@ -21,7 +21,7 @@ OBJ_CONF = 0.4  #
 
 #   YOLOv3
 cfgfile = "cfg/yolov3-dota.cfg"
-weightfile = "/mnt/share1/tangguijian/DOTA_YOLOv3_patch_AT/weights/yolov3-dota_110000.weights"
+weightfile = "yolov3-dota_110000.weights"
 
 #   Faster R-CNN
 
